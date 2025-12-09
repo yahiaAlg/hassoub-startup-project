c0015d2cd65554b9eb3200baff9b6237544c599d
yahiaAlg <yahialinus21alg@gmail.com>
2025-12-09 08:43:35 +0100

    Refactor code structure for improved readability and maintainability

--- populate_db.sh
+++ populate_db.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Populate database with initial data
+# python manage.py populate_lessons
 python manage.py populate_pages
-python manage.py populate_lessons
+python manage.py enhanced_lessons
 python manage.py populate_scenarios
 python manage.py populate_achievements
 python manage.py createsuperuser 
