4b5bb24999995423a82ecbe34b2b0df560fefdf3
yahiaAlg <yahialinus21alg@gmail.com>
2025-12-15 09:40:58 +0100

    chore: Update database settings to include character set and collation options

--- bizventure_kids/settings.py
+++ bizventure_kids/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES', character_set_connection=utf8mb4, collation_connection=utf8mb4_unicode_ci",
         },
     }
 }
