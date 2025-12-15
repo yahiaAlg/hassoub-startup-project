143071ae2502d6b0085f90c80a7ed33459b972d9
yahiaAlg <yahialinus21alg@gmail.com>
2025-12-15 09:26:05 +0100

    chore: Update database settings to use MySQL and add mysqlclient to requirements

--- bizventure_kids/settings.py
+++ bizventure_kids/settings.py
@@ -58,10 +58,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "bizventure_kids.wsgi.application"
 
 # Database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ndekhwek_hassoub_db",
+        "USER": "ndekhwek_admin",
+        "PASSWORD": "system2025*",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
